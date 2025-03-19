121cfc58f6c707e84cf07f9c348aa9cc5a326142
EddieTunji <davidlundi24@gmail.com>
2025-03-19 09:43:12 +0300

    binding eventListener

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
-}
+
+function alertAlert() {
+    alert('I was clicked!');
+  }
+  
+  function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', alertAlert);
+  }
+  
+  module.exports = {addingEventListener};
